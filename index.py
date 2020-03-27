bc575bc2111fdbab5713ba7a32ec207dd420a972
yeferson garcia <garciayeferson@gmail.com>
2020-03-27 04:03:08 -0500

    prueeba

---
+++ index.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import re
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('Index.html')
+
+
+@app.route('/about')
+def adout():
+    return 'hello 2'
+
+
+@app.route('/exp', methods=['POST'])
+def exp():
+
+    if request.method == 'POST':
+        expresion = request.form['expre']
+        patron = re.compile("[A-Z]{1}[0-9]{3}[a-z]{3}")
+        if patron.search(expresion):
+            resultado = patron.search(expresion).group(0)
+            if resultado == expresion :
+              return render_template('correcta.html')
+            else:
+               return render_template('incorrecta.html')
+        else:
+            return render_template('incorrecta.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
